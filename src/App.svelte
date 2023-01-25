63360c2203dcacfdd4c255598b260c6f86192e8f
Aaron Pritzlaff <aaron.pritzlaff.freelancer@theguardian.com>
2023-01-25 16:20:01 +0000

    clicking

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script>
 	import MaterialButton from "./MaterialButton.svelte";
+
+	const click = (event) => {
+		//https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+		const src = event.target || event.srcElement
+
+		alert(src.innerText);
+	}
 </script>
 
-<MaterialButton />
+<MaterialButton id="one" text="first" on:click={click}/>
+<MaterialButton id="two" text="second" on:click={click}/>
 
 <style global lang="scss">
 @use "@material/button/styles";
--- src/MaterialButton.svelte
+++ src/MaterialButton.svelte
@@ -1,13 +1,18 @@
-<script lang="ts">
+<script >
+
     import {onMount} from "svelte"
 
     import {MDCRipple} from '@material/ripple';
     onMount(() => {
-        const buttonRipple =  new MDCRipple(document.querySelector(".mdc-button"));
+        // const buttonRipple =  new MDCRipple(document.querySelector(".mdc-button"));
+        const buttonRipple =  new MDCRipple(document.getElementById(id));
     })
+
+    export let id;
+    export let text;
 </script>
 
-<button class="mdc-button">
+<button {id} class="mdc-button" on:click>
     <span class="mdc-button__ripple"></span>
-    <span class="mdc-button__label">Text Button</span>
+    <span class="mdc-button__label">{text}</span>
 </button>
